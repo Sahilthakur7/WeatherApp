85d1d6554b89b3b4996e30d44f841c8001ffc944
SahilThakur7 <sahilrd07@gmail.com>
2017-11-01 16:39:36 +0530

    API work started

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+const API_KEY = '4d202b958ce0c14a5c698ade28a71e00';
+const ROOT_URL = 'http://samples.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+    const url=`${ROOT_URL}&q=${city},in`;
+    const request = axios.get(url);
+    return{
+        type: FETCH_WEATHER
+    };
+}

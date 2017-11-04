ea0884da47633555f7145f936068a7308f16a2a6
SahilThakur7 <sahilrd07@gmail.com>
2017-11-04 10:55:50 +0530

    resolved the api bug

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API_KEY = '4d202b958ce0c14a5c698ade28a71e00';
-const ROOT_URL = 'http://samples.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
+const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,20 +2,33 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends React.Component{
+    renderWeather(cityData){
+        return(
+            <tr>
+                <td>
+                    {cityData.city.name}
+                </td>
+            </tr>
+        );
+
+    }
     render(){
         return(
-        <div>
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
-                    </tr>
-                </thead>
-            </table>
-        </div>
+            <div>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>City</th>
+                            <th>Temperature</th>
+                            <th>Pressure</th>
+                            <th>Humidity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.weather.map(this.renderWeather)}
+                    </tbody>
+                </table>
+            </div>
         );
     }
 }

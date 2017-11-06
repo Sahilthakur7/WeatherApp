f52b754d40555e437cc54e5ae62ae3f13083154c
SahilThakur7 <sahilrd07@gmail.com>
2017-11-06 14:18:32 +0530

    charts without numerics added

---
+++ src/components/chart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Sparklines , SparklinesLine} from 'react-sparklines';
+
+export default (props) => {
+    return(
+        <div>
+
+            <Sparklines height={110} width={180} data={props.data}>
+                <SparklinesLine color={props.color} />
+            </Sparklines>
+
+        </div>
+    )
+}
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends React.Component{
     renderWeather(cityData){
+        const name= cityData.city.name;
+        const temps= cityData.list.map(weather => weather.main.temp);
+        const pressures= cityData.list.map(weather => weather.main.pressure);
+        const humidities= cityData.list.map(weather => weather.main.humidity);
+
+        console.log(temps);
         return(
-            <tr>
+            <tr key= {name}>
                 <td>
-                    {cityData.city.name}
+                    {name}
                 </td>
+                <td>    <Chart data={temps} color="orange" /></td>
+                <td>    <Chart data={pressures} color="green" /></td>
+                <td>    <Chart data={humidities} color="blue" /></td>
             </tr>
         );
 
